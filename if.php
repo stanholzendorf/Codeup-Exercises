1189d2c73098ac5aeb941ec3d11270710cea23a1
stanholzendorf <sholzendorf@mail.com>
2016-02-29 10:12:30 -0600

    if UPDATE

--- if.php
+++ if.php
@@ -1,3 +1,47 @@
+
+
 <?php
 
-echo "This is the if exercise!";
+$a = 5;
+$b = 10;
+$c = '10';
+
+if ($a < $b) {
+    // TODO: Replace the blanks with the correct description
+    echo "$a is smaller than $b\n";
+}
+
+if ($b > $a) {
+    // TODO: Replace the blanks with the correct description
+    echo "$b is bigger than $a\n";
+}
+
+if ($b >= $c) {
+    // TODO: Replace the blanks with the correct description
+    echo "$b is equal or greater than $c\n";
+}
+
+if ($b <= $c) {
+    // TODO: Replace the blanks with the correct description
+    echo "$b is less than or equal to $c\n";
+}
+
+if ($b == $c) {
+    // TODO: Replace the blanks with the correct description
+    echo "$b is equal to $c\n";
+}
+
+if ($b === $c) {
+    // TODO: Replace the blanks with the correct description
+    echo "$b is identical to  $c\n";
+}
+
+// TODO: Replace `true` with the correct comparison
+if ($b != $c) {
+    echo "$b is not equal to $c\n";
+}
+
+// TODO: Replace `true` with the correct comparison
+if ($b !== $c) {
+    echo "$b is not identical to $c\n";
+}
