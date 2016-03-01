305f4b0153c2133b4ccafb34e7208849c9cb3926
stanholzendorf <sholzendorf@mail.com>
2016-03-01 15:22:58 -0600

    arithmetic (Functions in PHP)

---
+++ arithmetic.php
@@ -0,0 +1,35 @@
+<?php
+
+$a = 10;
+$b = 20;
+
+function add($a, $b)
+{
+    return $a + $b;
+}
+
+function subtract($a, $b)
+{
+    return $a - $b;
+}
+
+function multiply($a, $b)
+{
+    return $a * $b;
+}
+
+function divide($a, $b)
+{
+    return $a / $b;
+}
+
+function modulus($a, $b)
+{
+	return $a % $b;
+}
+
+echo add($a, $b) . PHP_EOL;
+echo subtract(25, 5) . PHP_EOL;
+echo multiply(5, 5) . PHP_EOL;
+echo divide(25, 5) . PHP_EOL;
+echo modulus(50, 5) . PHP_EOL;
