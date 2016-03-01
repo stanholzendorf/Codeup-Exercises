a300e214a0bd9ed4b9fd68c2aab16685e4e6c396
stanholzendorf <sholzendorf@mail.com>
2016-03-01 15:21:33 -0600

    Break and Continue Exercise.

---
+++ break_continue.php
@@ -0,0 +1,20 @@
+<?php
+
+for ($i = 1; $i <= 100; $i++) {
+
+	if ($i % 2 != 0) {
+		continue;
+		
+	} 
+
+	echo $i . "\n";
+}		
+
+
+for ($i =1; $i <= 100; $i++) {
+	if ($i == 11) {
+		break;
+	}
+
+	echo $i . "\n";
+}
